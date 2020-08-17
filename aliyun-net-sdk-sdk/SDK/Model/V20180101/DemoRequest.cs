60e79ff2bccf25cf70e77a9362e3708967e60ae9
sdk-team <sdk-team@alibabacloud.com>
2020-08-17 07:05:44 +0000

    Generated 2018-01-01 for SDK.

---
+++ aliyun-net-sdk-sdk/SDK/Model/V20180101/DemoRequest.cs
@@ -0,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.SDK;
+using Aliyun.Acs.SDK.Transform;
+using Aliyun.Acs.SDK.Transform.V20180101;
+
+namespace Aliyun.Acs.SDK.Model.V20180101
+{
+    public class DemoRequest : RoaAcsRequest<DemoResponse>
+    {
+        public DemoRequest()
+            : base("SDK", "2018-01-01", "Demo")
+        {
+			UriPattern = "/helloworld";
+			Method = MethodType.POST;
+        }
+
+		private string price;
+
+		private string name;
+
+		private List<ContentList> contentLists = new List<ContentList>(){ };
+
+		public string Price
+		{
+			get
+			{
+				return price;
+			}
+			set	
+			{
+				price = value;
+				DictionaryUtil.Add(QueryParameters, "Price", value);
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				return name;
+			}
+			set	
+			{
+				name = value;
+				DictionaryUtil.Add(BodyParameters, "Name", value);
+			}
+		}
+
+		public List<ContentList> ContentLists
+		{
+			get
+			{
+				return contentLists;
+			}
+
+			set
+			{
+				contentLists = value;
+				for (int i = 0; i < contentLists.Count; i++)
+				{
+					DictionaryUtil.Add(BodyParameters,"ContentList." + (i + 1) + ".Tag", contentLists[i].Tag);
+					for (int j = 0; j < contentLists[i].Letterss.Count; j++)
+					{
+						DictionaryUtil.Add(BodyParameters,"ContentList." + (i + 1) + ".Letters." +(j + 1), contentLists[i].Letterss[j]);
+					}
+				}
+			}
+		}
+
+		public class ContentList
+		{
+
+			private string tag;
+
+			private List<string> letterss = new List<string>(){ };
+
+			public string Tag
+			{
+				get
+				{
+					return tag;
+				}
+				set	
+				{
+					tag = value;
+				}
+			}
+
+			public List<string> Letterss
+			{
+				get
+				{
+					return letterss;
+				}
+				set	
+				{
+					letterss = value;
+				}
+			}
+		}
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override DemoResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return DemoResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
---
+++ aliyun-net-sdk-sdk/SDK/Transform/V20180101/DemoResponseUnmarshaller.cs
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.SDK.Model.V20180101;
+
+namespace Aliyun.Acs.SDK.Transform.V20180101
+{
+    public class DemoResponseUnmarshaller
+    {
+        public static DemoResponse Unmarshall(UnmarshallerContext context)
+        {
+			DemoResponse demoResponse = new DemoResponse();
+
+			demoResponse.HttpResponse = context.HttpResponse;
+			demoResponse.RequestId = context.StringValue("Demo.RequestId");
+			demoResponse.TetMap = context.StringValue("Demo.TetMap");
+
+			List<Dictionary<string, string>> demoResponse_listMap = new List<Dictionary<string, string>>();
+			for (int i = 0; i < context.Length("Demo.ListMap.Length"); i++) {
+				Dictionary<string, string> tmp = new Dictionary<string, string>() { };
+				foreach (var _item in context.ResponseDictionary){
+					string prefix = "Demo.ListMap["+ i +"].";
+					if (_item.Key.IndexOf(prefix) == 0){
+						tmp.Add(_item.Key.Substring(prefix.Length), _item.Value);
+					}
+				}
+				if (tmp.Count > 0){
+					demoResponse_listMap.Add(tmp);
+				}
+			}
+			demoResponse.ListMap = demoResponse_listMap;
+        
+			return demoResponse;
+        }
+    }
+}
