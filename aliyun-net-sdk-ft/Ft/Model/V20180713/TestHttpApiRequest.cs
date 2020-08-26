443b5f0619d81ae6f8ae3558aeab1db6320fdc1d
sdk-team <sdk-team@alibabacloud.com>
2020-08-26 06:01:10 +0000

    Generated 2018-07-13 for Ft.

--- aliyun-net-sdk-ft/Ft/Model/V20180713/TestHttpApiRequest.cs
+++ aliyun-net-sdk-ft/Ft/Model/V20180713/TestHttpApiRequest.cs
@@ -42,8 +42,6 @@ namespace Aliyun.Acs.Ft.Model.V20180713
 			Method = MethodType.POST;
         }
 
-		private string ispSignatureSecretKey;
-
 		private string stringValue;
 
 		private string otherParam;
@@ -51,21 +49,6 @@ namespace Aliyun.Acs.Ft.Model.V20180713
 		private bool? booleanParam;
 
 		private string defaultValue;
-
-		private string ispSignature;
-
-		public string IspSignatureSecretKey
-		{
-			get
-			{
-				return ispSignatureSecretKey;
-			}
-			set	
-			{
-				ispSignatureSecretKey = value;
-				DictionaryUtil.Add(QueryParameters, "IspSignatureSecretKey", value);
-			}
-		}
 
 		public string StringValue
 		{
@@ -76,7 +59,7 @@ namespace Aliyun.Acs.Ft.Model.V20180713
 			set	
 			{
 				stringValue = value;
-				DictionaryUtil.Add(BodyParameters, "StringValue", value);
+				DictionaryUtil.Add(QueryParameters, "StringValue", value);
 			}
 		}
 
@@ -89,7 +72,7 @@ namespace Aliyun.Acs.Ft.Model.V20180713
 			set	
 			{
 				otherParam = value;
-				DictionaryUtil.Add(BodyParameters, "OtherParam", value);
+				DictionaryUtil.Add(QueryParameters, "OtherParam", value);
 			}
 		}
 
@@ -115,20 +98,7 @@ namespace Aliyun.Acs.Ft.Model.V20180713
 			set	
 			{
 				defaultValue = value;
-				DictionaryUtil.Add(BodyParameters, "DefaultValue", value);
-			}
-		}
-
-		public string IspSignature
-		{
-			get
-			{
-				return ispSignature;
-			}
-			set	
-			{
-				ispSignature = value;
-				DictionaryUtil.Add(QueryParameters, "IspSignature", value);
+				DictionaryUtil.Add(QueryParameters, "DefaultValue", value);
 			}
 		}
 
